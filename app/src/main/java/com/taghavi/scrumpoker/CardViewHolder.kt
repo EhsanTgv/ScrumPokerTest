aced886d66b7f41308b9ce52a29b33d716c9071b
EhsanTgv <EhsanTgv@gmail.com>
2021-01-29 23:10:51 +0330

    Implement CardAdapter

--- app/src/main/java/com/taghavi/scrumpoker/CardViewHolder.kt
+++ app/src/main/java/com/taghavi/scrumpoker/CardViewHolder.kt
@@ -1,9 +1,10 @@
-package com.taghavi.scrumpoker.viewHolder
+package com.taghavi.scrumpoker
 
 import androidx.recyclerview.widget.RecyclerView
+import com.taghavi.scrumpoker.databinding.ItemCardBinding
 
 class CardViewHolder(private val binding: ItemCardBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(card: Card, clickListener: CardClickListener) {
+    fun bind(card: Card, clickListener: CardAdapter.CardClickListener) {
         binding.card = card
         binding.listener = clickListener
         binding.executePendingBindings()
